7a4564a383bf15dbce597a651c19c56eacaa7d13
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-02-28 17:01:00 +0100

    build the game board

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,11 @@
+import react from "../assets/react.svg"
+
+
 export default function Card() {
+
   return (
     <div className="card">
-      
+      <img src={react} className="logo"/>
     </div>
   )
 }

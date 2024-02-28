3e571aeecf6ae318d57eb273290f40a5e4f8b813
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-02-28 16:18:20 +0100

    build the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Board from "./components/Board"
+import Footer from "./components/Footer"
 
 import "./index.css"
 
 export default function App() {
 
   return (
-    <>
+    <div className="game">
       <h1>Memory Game</h1>
       <Board />
-    </>
+      <Footer />
+    </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <h1> 1 Moves</h1>
+  )
+}

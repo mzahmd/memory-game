d11d8cabc47ea2dcb9f5a97fcf6a35690e926f61
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-02-29 16:58:52 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,49 @@
-// import Board from "./components/Board"
+import Board from "./components/Board"
 import Footer from "./components/Footer"
-import Card from "./components/Card"
 import value from "./data/value.json"
 
 import { useEffect, useState } from "react"
 
 import "./index.css"
 
+interface CardChoice {
+  choiceOne: { id: number; value: number } | null
+  choiceTwo: { id: number; value: number } | null
+}
+
 export default function App() {
   const [cards, setCards] = useState([{ value: 0, id: 0, matched: false }])
-  const [choiceOne, setChoiceOne] = useState<{ id: number; value: number } | null>(null)
-  const [choiceTwo, setChoiceTwo] = useState<{ id: number; value: number } | null>(null)
+  const [choice, setChoice] = useState<CardChoice>({} as CardChoice)
   const [turns, setTurns] = useState(0)
   const [disabled, setDisabled] = useState(false)
 
   function handleClick(card: { id: number, value: number }) {
-    if (!disabled) {
-      choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    if (!disabled && card != choice.choiceOne && card != choice.choiceTwo) {
+      choice.choiceOne ? setChoice({ ...choice, choiceTwo: card }) : setChoice({ ...choice, choiceOne: card })
     }
   }
 
   function resetTurn() {
-    setChoiceOne(null);
-    setChoiceTwo(null);
+    setChoice({} as CardChoice);
     setDisabled(false)
   }
 
   useEffect(() => {
-    if (choiceOne && choiceTwo) {
+    if (choice.choiceOne && choice.choiceTwo) {
       setDisabled(true)
       setTurns(prevTurns => prevTurns + 1)
-      if (choiceOne.value === choiceTwo.value) {
+      if (choice.choiceOne.value === choice.choiceTwo.value) {
         setCards(prevCards => prevCards.map((card) => {
-          if (card.id === choiceOne.id || card.id === choiceTwo.id) {
+          if (card.id === choice.choiceOne?.id || card.id === choice.choiceTwo?.id) {
             return { ...card, matched: true }
           }
           return card
         }))
-        resetTurn()
       }
       setTimeout(() => resetTurn(), 1000)
     }
 
-  }, [choiceOne, choiceTwo])
+  }, [choice.choiceOne, choice.choiceTwo])
 
   useEffect(() => {
     const previousCards = [...value].sort(() => Math.random() - 0.5)
@@ -52,13 +53,7 @@ export default function App() {
   return (
     <div className="game">
       <h1>Memory Game</h1>
-      {/* <Board cards={cards} handleClick={handleClick} flipped={false} /> */}
-      <div className="board">
-        {cards.map((card) => {
-          const flipped = choiceOne?.id === card.id || choiceTwo?.id === card.id || card.matched
-          return <Card key={card.id} value={card.value} flipped={flipped} card={card} handleClick={handleClick} />
-        })}
-      </div>
+      <Board cards={cards} handleClick={handleClick} choiceOne={choice.choiceOne} choiceTwo={choice.choiceTwo} />
       <Footer turns={turns} />
     </div>
   )
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,18 +6,19 @@ export interface ICard {
   matched: boolean
 }
 
-
 interface Props {
   cards: ICard[]
-  flipped: boolean
+  choiceOne: { id: number; value: number; } | null
+  choiceTwo: { id: number; value: number; } | null
   handleClick: (card: ICard) => void
 }
 
-export default function Board({ cards, flipped, handleClick }: Props) {
+export default function Board({ cards, choiceOne, choiceTwo, handleClick }: Props) {
 
   return (
     <div className="board">
       {cards.map((card) => {
+        const flipped = choiceOne?.id === card.id || choiceTwo?.id === card.id || card.matched
         return <Card key={card.id} value={card.value} flipped={flipped} card={card} handleClick={handleClick} />
       })}
     </div>
